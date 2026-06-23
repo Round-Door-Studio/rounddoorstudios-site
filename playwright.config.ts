fe714d7e0f9fc3237a4ec530c407b791d1621a78
lanseafood <l.mao.ftwin@gmail.com>
2026-06-22 23:10:16 -0700

    4 workers locally and 2 on github

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 1,
-  workers: process.env.CI ? 2 : 2,
+  workers: process.env.CI ? 2 : 4,
   timeout: 60_000,
   expect: { timeout: 15_000 },
   reporter: 'html',
